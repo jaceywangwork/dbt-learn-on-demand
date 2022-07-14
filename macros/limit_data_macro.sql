98a052c139199ba6e13f2fe622f9c67fa0a6eb69
jaceywangwork <68527112+jaceywangwork@users.noreply.github.com>
2022-07-13 22:05:48 -0400

    add comment

--- macros/limit_data_macro.sql
+++ macros/limit_data_macro.sql
@@ -1,6 +1,9 @@
 {% macro limit_data_in_dev(column_name) %}
 
 {% if target.name == 'dev_jwang' %}
-where {{column_name}} >=dateadd('day', -3, current_timestamp)
+where {{column_name}} >=cast(dateadd('day', -3, getdate()) as date)
 {% endif %}
 {% endmacro %}
+
+
+--redshift do not use currentdate, currenttimstamp
