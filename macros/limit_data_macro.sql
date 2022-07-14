3a8802f5ffaa2d43a3d6d011c9ca1cbd575b6ba4
jaceywangwork <68527112+jaceywangwork@users.noreply.github.com>
2022-07-13 22:07:39 -0400

    add dynamic

--- macros/limit_data_macro.sql
+++ macros/limit_data_macro.sql
@@ -7,3 +7,17 @@ where {{column_name}} >=cast(dateadd('day', -3, getdate()) as date)
 
 
 --redshift do not use currentdate, currenttimstamp
+
+--assign dynamic dev day
+
+--uncomment below to run
+-- {% macro limit_data_in_dev(column_name,dev_day_of_data=3) %}
+
+-- {% if target.name == 'dev_jwang' %}
+-- where {{column_name}} >=cast(dateadd('day', -{{dev_day_of_data}}, getdate()) as date)
+
+-- {% endif %}
+
+-- {% endmacro %}
+
+
